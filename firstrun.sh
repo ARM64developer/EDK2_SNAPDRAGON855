5ab487c51935ad7e2cf9e6b90c46a3692aaa949e
sunflower2333 <54024877+sunflower2333@users.noreply.github.com>
2021-06-14 14:36:43 +0800

    Update firstrun.sh
    Fix underlying compilation issues with BaseTools

--- firstrun.sh
+++ firstrun.sh
@@ -4,4 +4,5 @@
 # https://github.com/tianocore/tianocore.github.io/wiki/Using-EDK-II-with-Native-GCC#Install_required_software_from_apt
 set -e
 . build_common.sh
-make -C ../edk2/BaseTools BUILD_CFLAGS=-W
+make -C ../edk2/BaseTools
+# BUILD_CFLAGS=-W
